8a01f3b4a39bcb12e3485d72819e893d2c53a688
Eden Meshualm <edenmeshulam97@gmail.com>
2023-01-23 10:31:23 +0200

    basic start

--- App.tsx
+++ App.tsx
@@ -1,33 +1,15 @@
 import 'react-native-gesture-handler';
 import * as React from 'react'
 
-import { NavigationContainer } from '@react-navigation/native';
 import { View } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import Home from './src/screens/Home/Home';
-import Iot from './src/screens/Iot/Iot';
-
-const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
-
+import {NativeBaseProvider} from "native-base"
+import Main from './src/screens/Main/Main';
 
 const App = ()=>{
 return(
-<NavigationContainer>
-{
-  //   <Stack.Navigator >
-  //   <Stack.Screen name="Home" component={Home} />
-  //   <Stack.Screen name="Iot" component={Iot} />
-
-  // </Stack.Navigator>
-  <Drawer.Navigator>
-  <Drawer.Screen name="Home" component={Home} />
-  <Drawer.Screen name="Iot" component={Iot} />
-</Drawer.Navigator>
-}
-
-</NavigationContainer>
+  <NativeBaseProvider>
+    <Main/>
+  </NativeBaseProvider>
 )
 }
 
--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,9 +1,24 @@
 import { Text, View } from "react-native"
 import React from 'react'
+import { NavigationContainer } from "@react-navigation/native"
+import Home from '../Home/Home';
+import Iot from '../Iot/Iot';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
 
 const Main = ()=>{
 return(
- <View></View>
+    <NavigationContainer>
+    {
+      <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={Home}/>
+      <Drawer.Screen name="Iot" component={Iot} />
+    </Drawer.Navigator>
+    }
+    
+    </NavigationContainer>
 )
 }
 
